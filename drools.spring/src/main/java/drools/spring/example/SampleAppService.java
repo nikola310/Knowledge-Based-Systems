cb14e38708bb06047bb8802643f0cd015748d820
Nikola <nikola_stojanovic1@yahoo.com>
2018-07-18 15:37:58 +0200

    Dodan primer sa vežbi.

---
+++ drools.spring/src/main/java/drools/spring/example/SampleAppService.java
@@ -0,0 +1,33 @@
+package drools.spring.example;
+
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import drools.spring.example.facts.Item;
+
+
+@Service
+public class SampleAppService {
+
+	private static Logger log = LoggerFactory.getLogger(SampleAppService.class);
+
+    private final KieContainer kieContainer;
+   
+    @Autowired
+    public SampleAppService(KieContainer kieContainer) {
+        log.info("Initialising a new example session.");
+        this.kieContainer = kieContainer;
+    }
+    
+    public Item getClassifiedItem(Item i) {
+        KieSession kieSession = kieContainer.newKieSession("ExampleSession");
+        kieSession.insert(i);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return i;
+    }
+}
