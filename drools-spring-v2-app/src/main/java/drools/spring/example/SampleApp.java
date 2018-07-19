4ae4615a293c86b0239a012deb3500f64a1b59fc
Nikola <nikola_stojanovic1@yahoo.com>
2018-07-19 15:49:38 +0200

    Dodan konceptualni model i primer sa vežbi.

---
+++ drools-spring-v2-app/src/main/java/drools/spring/example/SampleApp.java
@@ -0,0 +1,49 @@
+package drools.spring.example;
+
+import java.util.Arrays;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class SampleApp {
+	
+	private static Logger log = LoggerFactory.getLogger(SampleApp.class);
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SampleApp.class, args); 
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+
+        StringBuilder sb = new StringBuilder("Application beans:\n");
+        for (String beanName : beanNames) {
+            sb.append(beanName + "\n");
+        }
+        log.info(sb.toString());
+	}
+	
+	@Bean
+    public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("drools-spring-v2","drools-spring-v2-kjar", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
+    }
+	/*
+	 * KieServices ks = KieServices.Factory.get();
+	 * KieContainer kContainer = ks.newKieContainer(ks.newReleaseId("drools-spring-v2","drools-spring-v2-kjar", "0.0.1-SNAPSHOT"));
+	 * KieScanner kScanner = ks.newKieScanner(kContainer);
+	 * kScanner.start(10_000);
+	 * KieSession kSession = kContainer.newKieSession();
+	 */
+}
